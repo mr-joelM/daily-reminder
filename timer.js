4b50182389e1992965953a8cce1756e08e477cc7
mr-joelM <meyerjoel01@gmail.com>
2022-02-03 11:10:51 +0000

    set daily timer and reminder and alert message

---
+++ timer.js
@@ -0,0 +1,9 @@
+const now = new Date()
+let millisecTillTime =
+  new Date(now.getFullYear(), now.getMonth(), now.getDate(), 07, 45, 0, 0) - now
+if (millisecTillTime < 0) {
+  millisecTillTime += 86400000 // it's after 07.45am, try 07.45am tomorrow.
+}
+setTimeout(function () {
+  alert('Hello Peak!')
+}, millisecTillTime)
