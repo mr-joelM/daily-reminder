7ccb4a787a9898ec963f8a82eb2197460d30d0b8
mr-joelM <meyerjoel01@gmail.com>
2022-02-04 13:51:55 +0000

    adding of console.log

--- timer.js
+++ timer.js
@@ -1,9 +1,10 @@
-const now = new Date()
+let now = new Date()
 let millisecTillTime =
   new Date(now.getFullYear(), now.getMonth(), now.getDate(), 07, 45, 0, 0) - now
 if (millisecTillTime < 0) {
   millisecTillTime += 86400000 // it's after 07.45am, try 07.45am tomorrow.
 }
 setTimeout(function () {
+  console.log('Hello Peak!')
   alert('Hello Peak!')
 }, millisecTillTime)
