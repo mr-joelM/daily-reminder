f98f092c5a618e3649f9519cc979d0376878cef0
mr-joelM <meyerjoel01@gmail.com>
2022-02-11 11:19:26 +0000

    tryout again

--- helloPeak.py
+++ helloPeak.py
@@ -1,8 +1,5 @@
-# python try 1
-# print("Hello Peak!")
 
-#python try 2
-with open("output.txt", "w") as file:
+with open("peak.txt", "w") as file:
     file.write("Hello Peak!") 
 
 
---
+++ helloWorld.py
@@ -0,0 +1,2 @@
+with open("world.txt", "w") as file:
+    file.write("Hello World!") 
